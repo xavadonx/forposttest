f4721afbe54184f1960efeb250446b71323b0c0e
zer <lexx8407@gmail.com>
2018-07-08 21:26:08 +0300

    Initial commit

---
+++ app/src/main/java/ua/forposttest/Fighter.java
@@ -0,0 +1,12 @@
+package ua.forposttest;
+
+public class Fighter {
+    private int id;
+    private float position_lon;
+    private float position_lat;
+    private int health;
+    private int type;
+    private String team;
+    private int clips;
+    private int ammo;
+}
---
+++ app/src/main/java/ua/forposttest/MainActivity.java
@@ -0,0 +1,63 @@
+package ua.forposttest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Mapbox.getInstance(this, getString(R.string.access_token));
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+}
