6b71d65a4390a779d50b7db8515d48d53b13cd97
Alex <lexx8407@gmail.com>
2018-07-09 16:28:47 +0300

    firebase_test

--- app/src/main/java/ua/forposttest/MainActivity.java
+++ app/src/main/java/ua/forposttest/MainActivity.java
@@ -1,14 +1,28 @@
 package ua.forposttest;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.gson.reflect.TypeToken;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.maps.MapView;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
+    public static final String FIREBASE_STORAGE = "test-452f8.appspot.com";
     private MapView mapView;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
         Mapbox.getInstance(this, getString(R.string.access_token));
         mapView = (MapView) findViewById(R.id.mapView);
         mapView.onCreate(savedInstanceState);
+        test();
     }
 
     @Override
@@ -59,5 +74,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         mapView.onSaveInstanceState(outState);
+    }
+
+    private void test() {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("fighters").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                Fighter fighter = dataSnapshot.getValue(Fighter.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+        StorageReference storageRef = FirebaseStorage.getInstance().getReferenceFromUrl("gs://test-452f8.appspot.com"); //.child(photoName);
     }
 }
