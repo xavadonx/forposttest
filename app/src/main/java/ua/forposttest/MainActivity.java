769acf5b6fabca8e9b703b742457cc15dfd575fb
Alex <lexx8407@gmail.com>
2018-07-11 17:41:39 +0300

    fragments_test

--- app/src/main/java/ua/forposttest/MainActivity.java
+++ app/src/main/java/ua/forposttest/MainActivity.java
@@ -1,5 +1,6 @@
 package ua.forposttest;
 
+import android.content.res.Configuration;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -28,119 +29,134 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private MapView mapView;
-    private DatabaseReference mDatabase;
-    private List<Fighter> fighters;
+//    private MapView mapView;
+//    private DatabaseReference mDatabase;
+//    private List<Fighter> fighters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Mapbox.getInstance(this, getString(R.string.access_token));
-        mapView = findViewById(R.id.mapView);
-        mapView.onCreate(savedInstanceState);
+
+        showFragments();
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        mapView.onStart();
-    }
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        mapView.onStart();
+//    }
+//
+//    @Override
+//    public void onResume() {
+//        super.onResume();
+//        mapView.onResume();
+////        test();
+//        showFragments();
+//    }
+//
+//    @Override
+//    public void onPause() {
+//        super.onPause();
+//        mapView.onPause();
+//    }
+//
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//        mapView.onStop();
+//    }
+//
+//    @Override
+//    public void onLowMemory() {
+//        super.onLowMemory();
+//        mapView.onLowMemory();
+//    }
+//
+//    @Override
+//    protected void onDestroy() {
+//        super.onDestroy();
+//        mapView.onDestroy();
+//    }
+//
+//    @Override
+//    protected void onSaveInstanceState(Bundle outState) {
+//        super.onSaveInstanceState(outState);
+//        mapView.onSaveInstanceState(outState);
+//    }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        mapView.onResume();
-        test();
+    private void showFragments() {
+//        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.am_fragment, fragment, key)
+                .replace(R.id.container, MapFragment.newInstance())
                 .commit();
+//        } else {
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//                    .replace(R.id.land_container, ContactListFragment.newInstance(contacts))
+//                    .commit();
+//
+//            getSupportFragmentManager()
+//                    .beginTransaction()
+//                    .replace(R.id.land_det, ContactDetFragment.newInstance(contacts.get(0)))
+//                    .commit();
+//        }
     }
 
-    @Override
-    public void onPause() {
-        super.onPause();
-        mapView.onPause();
-    }
+//    private void test() {
+//        mDatabase = FirebaseDatabase.getInstance().getReference();
+//        mDatabase.child("fighters").addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                GenericTypeIndicator<List<Fighter>> indicator = new GenericTypeIndicator<List<Fighter>>() {
+//                };
+//                fighters = dataSnapshot.getValue(indicator);
+//                addMarkers();
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError databaseError) {
+//
+//            }
+//        });
+//    }
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        mapView.onStop();
-    }
+//    private List<Marker> markers;
 
-    @Override
-    public void onLowMemory() {
-        super.onLowMemory();
-        mapView.onLowMemory();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        mapView.onDestroy();
-    }
-
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        mapView.onSaveInstanceState(outState);
-    }
-
-    private void test() {
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-        mDatabase.child("fighters").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                GenericTypeIndicator<List<Fighter>> indicator = new GenericTypeIndicator<List<Fighter>>() {
-                };
-                fighters = dataSnapshot.getValue(indicator);
-                addMarkers();
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-        });
-    }
-
-    private List<Marker> markers;
-
-    private void addMarkers() {
-
-
-        for (Fighter fighter : fighters) {
-            mapView.getMapAsync(new OnMapReadyCallback() {
-                @Override
-                public void onMapReady(MapboxMap mapboxMap) {
-                    boolean sign = true;
-                    markers = mapboxMap.getMarkers();
-                    for (Marker m : markers) {
-                        if (Integer.parseInt(m.getTitle()) == fighter.id) {
-                            sign = false;
-                            break;
-                        }
-                    }
-
-                    if (sign) {
-                        mapboxMap.addMarker(new MarkerOptions()
-                                .position(new LatLng(fighter.position_lat, fighter.position_lon))
-                                .title(String.valueOf(fighter.id))
-                                .snippet(fighter.team)
-
-                        );
-                    } else {
-                        for (Marker m : markers) {
-                            if (Integer.parseInt(m.getTitle()) == fighter.id) {
-                                m.setPosition(new LatLng(fighter.position_lat, fighter.position_lon));
-                                break;
-                            }
-                        }
-                    }
-                }
-            });
-        }
-    }
+//    private void addMarkers() {
+//
+//
+//        for (Fighter fighter : fighters) {
+//            mapView.getMapAsync(new OnMapReadyCallback() {
+//                @Override
+//                public void onMapReady(MapboxMap mapboxMap) {
+//                    boolean sign = true;
+//                    markers = mapboxMap.getMarkers();
+//                    for (Marker m : markers) {
+//                        if (Integer.parseInt(m.getTitle()) == fighter.id) {
+//                            sign = false;
+//                            break;
+//                        }
+//                    }
+//
+//                    if (sign) {
+//                        mapboxMap.addMarker(new MarkerOptions()
+//                                .position(new LatLng(fighter.position_lat, fighter.position_lon))
+//                                .title(String.valueOf(fighter.id))
+//                                .snippet(fighter.team)
+//
+//                        );
+//                    } else {
+//                        for (Marker m : markers) {
+//                            if (Integer.parseInt(m.getTitle()) == fighter.id) {
+//                                m.setPosition(new LatLng(fighter.position_lat, fighter.position_lon));
+//                                break;
+//                            }
+//                        }
+//                    }
+//                }
+//            });
+//        }
+//    }
 }
--- app/src/main/java/ua/forposttest/MapFragment.java
+++ app/src/main/java/ua/forposttest/MapFragment.java
@@ -1,108 +1,163 @@
 package ua.forposttest;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.Marker;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link MapFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link MapFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.List;
+import java.util.Objects;
+
 public class MapFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private MapView mapView;
+    private DatabaseReference mDatabase;
+    private List<Marker> markers;
+    private List<Fighter> fighters;
 
-    private OnFragmentInteractionListener mListener;
-
-    public MapFragment() {
-        // Required empty public constructor
+    public static MapFragment newInstance() {
+        return new MapFragment();
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MapFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MapFragment newInstance(String param1, String param2) {
-        MapFragment fragment = new MapFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_map, container, false);
+        Mapbox.getInstance(Objects.requireNonNull(getContext()), getString(R.string.access_token));
+        mapView = root.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+
+        return root;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+        getDataFromFB();
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    private void getDataFromFB() {
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase.child("fighters").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                GenericTypeIndicator<List<Fighter>> indicator = new GenericTypeIndicator<List<Fighter>>() {
+                };
+                fighters = dataSnapshot.getValue(indicator);
+                addMarkers();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void addMarkers() {
+//        for (Fighter fighter : fighters) {
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                markers = mapboxMap.getMarkers();
+
+                for (Fighter fighter : fighters) {
+                    boolean sign = true;
+                    for (Marker m : markers) {
+                        if (Integer.parseInt(m.getTitle()) == fighter.id) {
+                            m.setPosition(new LatLng(fighter.position_lat, fighter.position_lon));
+                            sign = false;
+                            break;
+                        }
+                    }
+                    if (sign) {
+                        mapboxMap.addMarker(new MarkerOptions()
+                                .position(new LatLng(fighter.position_lat, fighter.position_lon))
+                                .title(String.valueOf(fighter.id))
+                                .snippet(fighter.team)
+                        );
+                    }
+                }
+            }
+        });
+    }
+
+
+//                    boolean sign = true;
+//                    markers = mapboxMap.getMarkers();
+//                    for (Marker m : markers) {
+//                        if (Integer.parseInt(m.getTitle()) == fighter.id) {
+//                            sign = false;
+//                            break;
+//                        }
+//                    }
+//
+//                    if (sign) {
+//                        mapboxMap.addMarker(new MarkerOptions()
+//                                .position(new LatLng(fighter.position_lat, fighter.position_lon))
+//                                .title(String.valueOf(fighter.id))
+//                                .snippet(fighter.team)
+//
+//                        );
+//                    } else {
+//                        for (Marker m : markers) {
+//                            if (Integer.parseInt(m.getTitle()) == fighter.id) {
+//                                m.setPosition(new LatLng(fighter.position_lat, fighter.position_lon));
+//                                break;
+//                            }
+//                        }
+//                    }
+//    }
+//});
+//        }
+//        }
 }
